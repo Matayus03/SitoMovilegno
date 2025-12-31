3082628cd0f3512755f19071d0e8d5524393c754
Matayus03 <colbalchiniteo@gmail.com>
2025-12-31 10:47:13 +0000

    Add files via upload

--- JS/menu-toggle.js
+++ JS/menu-toggle.js
@@ -5,3 +5,13 @@ menuToggle.addEventListener("click", () => {
     menu.classList.toggle("attivo");
     document.body.classList.toggle("menu-attivo");
 });
+
+document.addEventListener("click", (e) => {
+    const isClickInsideMenu = menu.contains(e.target);
+    const isClickOnToggle = menuToggle.contains(e.target);
+
+    if (!isClickInsideMenu && !isClickOnToggle && menu.classList.contains("attivo")) {
+        menu.classList.remove("attivo");
+        document.body.classList.remove("menu-attivo");
+    }
+});
